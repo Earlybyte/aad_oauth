b59cb485784904eaf90f475b763c2ad5486e4927
Steven Butler <steven.butler@kjr.com.au>
2019-06-09 19:54:04 +1000

    Cleanup unused imports and fix test config params

--- lib/request_code.dart
+++ lib/request_code.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:ui';
-import 'package:flutter/widgets.dart';
 import 'request/authorization_request.dart';
 import 'model/config.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
--- test/aad_oauth_test.dart
+++ test/aad_oauth_test.dart
@@ -4,14 +4,15 @@ import 'package:aad_oauth/aad_oauth.dart';
 import 'package:aad_oauth/model/config.dart';
 
 void main() {
-  test('adds one to input values', () {
+  test('Token is not valid before signing in', () {
     final Config config = new Config(
-      "YOUR TENANT ID",
-      "YOUR CLIENT ID",
-      "openid profile offline_access");
+        "YOUR TENANT ID",
+        "YOUR CLIENT ID",
+        "openid profile offline_access",
+        "https://login.live.com/oauth20_desktop.srf");
     final AadOAuth oauth = new AadOAuth(config);
 
+    expect(oauth.tokenIsValid(), false);
     //TODO testing
-    
   });
 }
