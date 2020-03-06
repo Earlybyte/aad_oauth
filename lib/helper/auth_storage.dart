66cc1d0cbb7a49a53bcb6d03db1f614fafe9c57e
Erick Daros <erickdarosilva@gmail.com>
2020-03-06 17:06:40 -0300

    Added requested changes to auth_storage

--- lib/helper/auth_storage.dart
+++ lib/helper/auth_storage.dart
@@ -6,16 +6,20 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class AuthStorage {
   static AuthStorage shared = new AuthStorage();
   FlutterSecureStorage _secureStorage = FlutterSecureStorage();
-  final String _identifier = "Token";
+  String _tokenIdentifier;
+
+  AuthStorage({String tokenIdentifier = "Token"}) {
+    _tokenIdentifier = tokenIdentifier;
+  }
 
   Future<void> saveTokenToCache(Token token) async {
     var data = Token.toJsonMap(token);
     var json = Convert.jsonEncode(data);
-    await _secureStorage.write(key:_identifier, value: json);
+    await _secureStorage.write(key: _tokenIdentifier, value: json);
   }
 
   Future<T> loadTokenToCache<T extends Token>() async {
-    var json = await _secureStorage.read(key:_identifier);
+    var json = await _secureStorage.read(key: _tokenIdentifier);
     if (json == null) return null;
     try {
       var data = Convert.jsonDecode(json);
@@ -30,6 +34,6 @@ class AuthStorage {
       Token.fromJson(data);
 
   Future clear() async {
-    _secureStorage.delete(key:_identifier);
+    _secureStorage.delete(key: _tokenIdentifier);
   }
 }
